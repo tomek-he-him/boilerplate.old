da97dee01787545d2b7d97e37f1cda6c91d65d63
Tomek Wiszniewski <t.wiszniewski@gmail.com>
2015-07-31 12:09:03 +0200

    Allow no initial remote

--- _bootstrap.fish
+++ _bootstrap.fish
@@ -23,7 +23,7 @@ end
 
 echo \n'Doing the initial commit…'
 git remote rename origin boilerplate
-and git remote add origin $repo
+git remote add origin $repo
 and if test (count (git branch --list master)) -gt 0
   git branch -D master
   else; true
